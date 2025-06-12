02cdc2cf1e7c03721717f818217f6ac3558f43e6
SanketTops <sanket.tops@gmail.com>
2025-06-12 16:16:27 +0530

    String

---
+++ Assignments/Module-1/tsring.py
@@ -0,0 +1,36 @@
+mystr = "this is python!"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4])  # slice - range
+print(mystr[5:])
+print(mystr[:4])"""
+
+
+# ------------------------- #
+# print(mystr)
+# print(len(mystr))
+
+# -----------------------------#
+print(mystr)
+# print(mystr.strip())
+# print(mystr.lower())
+# print(mystr.upper())
+# print(mystr.replace("P", "p"))
+# print(mystr.replace("Python", "JAVA"))
+# print(mystr.capitalize())
+# print(mystr.casefold())
+# print(mystr.count("i"))
+# print(mystr.count("p"))
+
+# print(mystr.startswith("this"))
+# print(mystr.endswith("!"))
+# print(mystr.find("p"))
+# print(mystr.index("python"))
+
+# print(mystr.isalpha())
+# print(mystr.isdigit())
+# print(mystr.isalnum())
+# print(mystr.islower())
+# print(mystr.isupper())
+print(mystr.title())
---
+++ Assignments/Module-1/while_table.py
@@ -0,0 +1,10 @@
+"""i = 1
+while i <= 10:
+    print(f"5*{i}={5 * i}")
+    i += 1"""
+
+n = int(input("Enter any number for Table:"))
+i = 1
+while i <= 10:
+    print(f"{n}*{i}={n * i}")
+    i += 1
