6df9be6fae56a6ceb8d9b7031edb73014d7700d4
SanketTops <sanket.tops@gmail.com>
2025-10-01 17:37:10 +0530

    Auth

--- Projects/FinalProject/userapp/urls.py
+++ Projects/FinalProject/userapp/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('notes/',views.notes,name='notes'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
-    path('signin/',views.signin,name='sigin'),
+    path('signin/',views.signin,name='signin'),
     path('signup/',views.signup,name='signup'),
+    path('userlogout/',views.userlogout,name='userlogout'),
     
 ]
--- Projects/FinalProject/userapp/views.py
+++ Projects/FinalProject/userapp/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib.auth import logout
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    user=request.session.get("user")
+    return render(request,'index.html',{'user':user})
 
 def notes(request):
     return render(request,'notes.html')
@@ -15,7 +18,31 @@ def contact(request):
     return render(request,'contact.html')
 
 def signin(request):
+    if request.method=='POST':
+        em=request.POST["email"]
+        pa=request.POST["password"]
+        
+        user=UserSignup.objects.filter(email=em,password=pa)
+        if user:
+            print("Login Successfully!")
+            request.session["user"]=em
+            return redirect("/")
+        else:
+            print("Error!Login Faild...")
     return render(request,'signin.html')
 
 def signup(request):
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("Signup Successfully!")
+            return redirect("signin")
+        else:
+            print(form.errors)
     return render(request,'signup.html')
+
+def userlogout(request):
+    logout(request)
+    return redirect('signin')
+    
