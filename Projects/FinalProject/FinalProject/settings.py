c2dec59b272a43a79fd29420b52955df89da75a7
SanketTops <sanket.tops@gmail.com>
2025-11-07 16:41:21 +0530

    Final Changes

--- Projects/FinalProject/FinalProject/settings.py
+++ Projects/FinalProject/FinalProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3e^dgac!-*q9!&rzd1-1%yhsh*8)rki8hryvn0$!wz)26iu=_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
